c4ead2747c7d4198f55d5525914ae009f595ab26
idaifish <idaifish@gmail.com>
2018-06-08 15:59:18 +0800

    add query_string, request_uri, remote_addr.

--- rasp4php/core/hooks/v5/php_mysqlnd_cmd_write.js
+++ rasp4php/core/hooks/v5/php_mysqlnd_cmd_write.js
@@ -17,6 +17,18 @@
     message.filename = Memory.readCString(ptr(getFilename()));
     message.lineno = getLineno();
 
+    var sapi_getenv_addr = Module.findExportByName(null, 'sapi_getenv');
+    var sapi_getenv = new NativeFunction(sapi_getenv_addr, 'pointer', ['pointer', 'int']);
+    var envArray = ['SERVER_ADDR', 'SERVER_NAME', 'QUERY_STRING', 'DOCUMENT_ROOT', 'REMOTE_ADDR', 'REQUEST_URI'];
+    var getenv = function (env) {
+        var envName= Memory.allocUtf8String(env);
+        var envValue = sapi_getenv(envName, env.length);
+        return Memory.readCString(envValue);
+    }
+    message.query_string = getenv("QUERY_STRING");
+    message.remote_addr = getenv("REMOTE_ADDR");
+    message.request_uri = getenv("REQUEST_URI");
+
     var sql = Memory.readCString(Memory.readPointer(args[0].add(32)));
     if (sql !== null) {
       message.args.push(sql);
